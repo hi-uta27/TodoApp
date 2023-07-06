44ba79506ae189d203d33f3d0fafb5c28577c019
hi-uta27 <tavanhieun@gmail.com>
2023-07-06 16:32:02 +0700

    create task title screen

---
+++ TodoApp/TodoApp/Features/Task/Presentations/TaskTitle/TaskTitleViewController.swift
@@ -0,0 +1,14 @@
+//
+//  TaskTitleViewController.swift
+//  TodoApp
+//
+//  Created by TaHieu on 7/6/23.
+//
+
+import Foundation
+
+class TaskTitleViewController: BaseViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
