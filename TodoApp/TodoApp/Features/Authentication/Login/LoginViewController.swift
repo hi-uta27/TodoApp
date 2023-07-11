a9ed330ccbf3bf2ab04030cdc406642a36504379
hi-uta27 <tavanhieun@gmail.com>
2023-07-11 15:32:39 +0700

    validate user account

--- TodoApp/TodoApp/Features/Authentication/Login/LoginViewController.swift
+++ TodoApp/TodoApp/Features/Authentication/Login/LoginViewController.swift
@@ -5,10 +5,49 @@
 //  Created by TaHieu on 7/11/23.
 //
 
-import Foundation
+import UIKit
 
 class LoginViewController: BaseViewController {
+    @IBOutlet private var userNameTextField: UITextField!
+    @IBOutlet private var passwordTextField: UITextField!
+
+    private var openRegisterScreen: (() -> Void)!
+
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+
+    @IBAction private func touchUpInsideLoginButton(_ sender: Any) {
+        do {
+            let (email, password) = try UserValidator.validateData(email: userNameTextField.text, password: passwordTextField.text)
+            login(with: email, password: password)
+        } catch {
+            showAlert(title: "Error", message: error.localizedDescription, actions: [.okAction()])
+        }
+    }
+
+    private func login(with email: String, password: String) {
+        print(Self.self, #function, email, password)
+    }
+
+    @IBAction private func touchUpInsideLoginWithGoogleButton(_ sender: Any) {
+        print(Self.self, #function)
+    }
+
+    @IBAction private func touchUpInsideLoginWithAppleButton(_ sender: Any) {
+        print(Self.self, #function)
+    }
+
+    @IBAction private func touchUpInsideRegisterButton(_ sender: Any) {
+        print(Self.self, #function)
+        openRegisterScreen?()
+    }
+}
+
+extension LoginViewController {
+    static func initial(openRegisterScreen: @escaping () -> Void) -> Self {
+        let viewController = initial()
+        viewController.openRegisterScreen = openRegisterScreen
+        return viewController
     }
 }
