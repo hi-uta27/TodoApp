6642b6851ea81ed1859e901659f3b64c862249c2
hi-uta27 <tavanhieun@gmail.com>
2023-07-16 16:51:48 +0700

    create protocol for change password remote data source

---
+++ TodoApp/TodoApp/Features/UserProfile/Data/DataSource/ChangePassword/ChangePasswordRemoteDataSource.swift
@@ -0,0 +1,12 @@
+//
+//  ChangePasswordRemoteDataSource.swift
+//  TodoApp
+//
+//  Created by TaHieu on 7/16/23.
+//
+
+import Foundation
+
+protocol ChangePasswordRemoteDataSource {
+    func changePassword(_ password: String, completed: @escaping (Error?) -> Void)
+}
