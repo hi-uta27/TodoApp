a03a294254aec2f6441b2b3bc509392f4eca2c6e
hi-uta27 <tavanhieun@gmail.com>
2023-07-16 13:47:15 +0700

    design change account name screen

---
+++ TodoApp/TodoApp/Features/UserProfile/ChangeAccountName/ChangeAccountNameViewController.swift
@@ -0,0 +1,14 @@
+//
+//  ChangeAccountNameViewController.swift
+//  TodoApp
+//
+//  Created by TaHieu on 7/16/23.
+//
+
+import Foundation
+
+class ChangeAccountNameViewController: BaseViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
